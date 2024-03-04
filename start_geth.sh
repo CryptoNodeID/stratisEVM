829d617bf9be2c3afe77c2510cbd0feacc7e8d7d
Billy Junaidi <billy.junaidi@gmail.com>
2024-03-04 12:19:40 +0800

    change rpc to localhost

--- start_geth.sh
+++ start_geth.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 if [ "$1" == "mainnet" ]; then
   nohup ./geth --http --http.api eth,net,engine,admin \
     --datadir=/appl/stratisEVM/data/mainnet/geth \
-    --authrpc.addr=$_ip \
-    --authrpc.jwtsecret=/appl/stratisEVM/config/mainnet/jwtsecret \
+    --authrpc.addr=127.0.0.1 \
+    --authrpc.jwtsecret=/appl/stratisEVM/config/mainnet/jwtsecret\
     --syncmode=full > geth_mainnet.out 2>&1 &
 fi
 nohup ./geth --auroria --http --http.api eth,net,engine,admin \
   --datadir=/appl/stratisEVM/data/testnet/geth \
-  --authrpc.addr=$_ip \
-  --authrpc.jwtsecret=/appl/stratisEVM/config/testnet/jwtsecret \
+  --authrpc.addr=127.0.0.1 \
+  --authrpc.jwtsecret=/appl/stratisEVM/config/testnet/jwtsecret\
   --syncmode=full > geth.out 2>&1 &
